b9cc3fe0ed3679367fdb540d4db784085ce21479
Lucky Arora <luckyarora.la123@gmail.com>
2025-03-12 11:49:29 +0530

    Solution to Print a Diamond Pattern

--- Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
+++ Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
@@ -1,29 +1,34 @@
-#include<stdio.h>
-int main(){
-   int n,i,j,k;
-   scanf("%d",&n);
-   for(i=1;i<=n;i++){
-      for(j=i;j<=n;j++){
-         printf(" ");
-      }
-      for(k=1;k<=i;k++){
-         printf("*");
-      }
-      for(k=1;k<i;k++){
-         printf("*");
-      }
-      printf("\n");
-   }
-   for(i=1;i<n;i++){
-      for(j=0;j<=i;j++){
-         printf(" ");
-      }
-      for(k=i;k<n;k++){
-         printf("*");
-      }
-      for(k=i;k<n-1;k++){
-         printf("*");
-      }
-      printf("\n");
-   }
+#include <stdio.h>
+
+int main() {
+    int n, i, j, k;
+    scanf("%d", &n);
+
+    // Upper part of the diamond
+    for (i = 1; i <= n; i++) {
+        // Print spaces
+        for (j = i; j < n; j++) {
+            printf(" ");
+        }
+        // Print stars (left half)
+        for (k = 1; k <= (2 * i - 1); k++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    // Lower part of the diamond
+    for (i = n - 1; i >= 1; i--) {
+        // Print spaces
+        for (j = n; j > i; j--) {
+            printf(" ");
+        }
+        // Print stars (right half)
+        for (k = 1; k <= (2 * i - 1); k++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
 }
